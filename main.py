28d4d80b92819acbb17593d8ac2c5041fbf2f464
Rachit Sharma <rachit995@rocketmail.com>
2021-07-01 09:44:34 +0530

    [FIX]: Fixed heroku procfile

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI
 import requests
 import json
+import socket
 
+hostname = socket.gethostname()
+VERSION = "0.1.1"
+PRECISION = ".20f"
 
 app = FastAPI()
 
 
-@app.get("/ticker")
+@app.get("/")
 async def root():
+    return {
+        "name": "trgi-ticker-service",
+        "host": hostname,
+        "version": VERSION
+    }
+
+
+@app.get("/ticker")
+async def ticker():
     url = "https://api.coingecko.com/api/v3/coins/the-real-golden-inu"
     response = requests.request("GET", url)
     data = json.loads(response.text)
     current = 0
-    current = format(data["tickers"][0]["converted_last"]["usd"], '.20f')
-    last = format(data["tickers"][0]["last"], '.20f')
+    current = format(data["tickers"][0]["converted_last"]["usd"], PRECISION)
     return {"current": current}
